b8588192b082cbc918c1bb9ea4508f69fb6ab9ce
Chakra-Z <13369267806@163.com>
2019-09-25 14:10:04 +0800

    revise test

--- java_9_21/src/Test.java
+++ java_9_21/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    static SeqList list = new SeqList();
+    public static SeqList list = new SeqList();
 
     public static void main(String[] args){
         listDisplay(); // 打印顺序表
@@ -15,12 +15,12 @@ public class Test {
     }
 
     // 打印顺序表
-    static void listDisplay() {
+    public static void listDisplay() {
         list.display();
     }
 
     // 在pos位置新增元素
-    static void listAdd() {
+    public static void listAdd() {
         list.add(0,1);
         list.add(1,3);
         list.add(2,5);
@@ -30,43 +30,43 @@ public class Test {
     }
 
     // 查找某个元素对应的位置
-    static void listSearch(){
+    public static void listSearch(){
         int result = list.search(5);
         System.out.println("result应该是 2，实际是 "+result);
     }
 
     // 判定是否包含某个元素
-    static void listContains() {
+    public static void listContains() {
         boolean result = list.contains(9);
         System.out.println("result应该是 true，实际是 "+result);
     }
 
     // 获取 pos 位置元素
-    static void listGetPos() {
+    public static void listGetPos() {
         int result = list.getPos(2);
         System.out.println("result应该是 5，实际是 "+result);
     }
 
     // 给 pos 位置元素设为 value
-    static void listSetPos() {
+    public static void listSetPos() {
         list.setPos(3,15);
         list.display();
     }
 
     // 删除第一次出现的关键字value
-    static void listRemove() {
+    public static void listRemove() {
         list.remove(9);
         list.display();
     }
 
     // 获取顺序表长度
-    static void listSize() {
+    public static void listSize() {
         int result = list.size();
         System.out.println("result应该是 5，实际是 "+result);
     }
     
     // 清空顺序表
-    static void listClear() {
+    public static void listClear() {
         list.clear();
         list.display();
     }
