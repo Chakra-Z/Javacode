08829cc6442d7fb0feede8800f92be28c3e98b18
Chakra-Z <13369267806@163.com>
2020-02-12 12:30:42 +0800

    add TCP

--- java_0212/src/tcp/TCPClient.java
+++ java_0212/src/tcp/TCPClient.java
@@ -1,4 +1,24 @@
 package tcp;
 
+import java.io.*;
+import java.net.Socket;
+
 public class TCPClient {
+
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 9999;
+
+    public static void main(String[] args) throws IOException {
+        // 建立了客户端到服务端的一个TCP连接
+        Socket socket = new Socket(HOST, PORT);
+        InputStream is = socket.getInputStream();// 输入字节流
+        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+
+        OutputStream os = socket.getOutputStream();// 输出字节流
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+        PrintWriter pw = new PrintWriter(os,true);
+        pw.println("hello,我来了");
+//        bw.write("我来了");
+//        bw.flush();
+    }
 }
--- java_0212/src/tcp/TCPServer.java
+++ java_0212/src/tcp/TCPServer.java
@@ -15,14 +15,24 @@ public class TCPServer {
             // 阻塞，等待新的客户端连接
             Socket socket = serverSocket.accept();
             // 处理这个业务可能发生阻塞
-            InputStream is = socket.getInputStream();
+            InputStream is = socket.getInputStream();// 输入字节流
             BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
 
-            OutputStream os = socket.getOutputStream();
+            OutputStream os = socket.getOutputStream();// 输出字节流
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+
+            // 1.先接受，并打印
+            // 2.响应，我已接收xxx消息
+
+            String line;
+            //=========02:47
+            while ((line = br.readLine()) != null){
+                System.out.println("服务器接收到数据："+line);
+                bw.write("我已经接收到了"+line+"消息");// 加换行符！！！
+                //bw.write("...")
+                // 刷新缓冲区
+                bw.flush();
+            }
         }
-
-
-
     }
 }
