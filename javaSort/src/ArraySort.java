f11ec391b2117b1af738a21093cb4eca871496fa
Chakra-Z <13369267806@163.com>
2020-04-10 22:35:18 +0800

    arraySort

---
+++ javaSort/src/ArraySort.java
@@ -0,0 +1,106 @@
+public class ArraySort {
+
+    private static void swap(int[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    // 插入排序
+    private static void insertSort(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            int v = arr[i];
+            int p = i;
+            while(p>0 && arr[p-1]>v){
+                arr[p] = arr[p-1];
+                p--;
+            }
+            arr[p] = v;
+        }
+    }
+
+    // 冒泡排序
+    private static void bubbleSort(int[] arr) {
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int k = 0; k < arr.length-i-1; k++){
+                if(arr[k] > arr[k+1]){
+                    swap(arr,k,k+1);
+                }
+            }
+        }
+    }
+
+    // 快速排序
+    private static void quickSort(int[] arr, int left, int right){
+        if(left >= right){
+            return;
+        }
+        int key = arr[left];
+        int l = left;
+        int r = right;
+        while(l < r){
+            while (l<r && arr[r]>key){
+                r--;
+            }
+            if(l < r){
+                //arr[left] = arr[right];
+                swap(arr,l,r);
+                l++;
+            }
+            while (l<r && arr[l]<key){
+                l++;
+            }
+            if(l < r){
+                //arr[right] = arr[left];
+                swap(arr,l,r);
+                r--;
+            }
+        }
+        //arr[left] = key;
+        quickSort(arr, left, l-1);
+        quickSort(arr, l+1, right);
+    }
+
+    // 堆排序
+    private static void heapSort(int[] tree, int n){
+        buildHeap(tree, n);
+        for(int i = n-1; i >= 0; i--) {
+            swap(tree, i, 0);
+            heapify(tree, 0, i);
+        }
+    }
+
+    private static void buildHeap(int[] tree, int n){
+        int lastNode = n-1;
+        int parent = (lastNode-1)/2;
+        for(int i = parent; i >= 0; i--) {
+            heapify(tree, i, n);
+        }
+    }
+    private static void heapify(int[] tree, int i, int n){
+        int c1 = 2*i+1;
+        int c2 = 2*i+2;
+        int max = i ;
+        if(c1 < n && tree[max] < tree[c1]){
+            max = c1;
+        }
+        if(c2 < n && tree[max] < tree[c2]){
+            max = c2;
+        }
+        if(max != i){
+            swap(tree, i, max);
+            heapify(tree, max, n);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] array = new int[]{2,9,8,7,6,4};
+        int n = array.length;
+        //quickSort(array,0,n - 1);
+        //heapify(array,0,7) ;
+        heapSort(array,n);
+        for (int i : array){
+            System.out.print(i + " ");
+        }
+    }
+}
+
